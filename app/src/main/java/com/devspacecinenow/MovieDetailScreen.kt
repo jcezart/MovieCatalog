e1abdb426d97895b6458541a32bb827ceaf5f19c
jcezart <jcezar.t@gmail.com>
2024-08-22 21:57:07 -0300

    consumo da API + UI

--- app/src/main/java/com/devspacecinenow/MovieDetailScreen.kt
+++ app/src/main/java/com/devspacecinenow/MovieDetailScreen.kt
@@ -29,6 +29,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
